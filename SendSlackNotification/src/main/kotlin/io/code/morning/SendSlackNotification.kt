cc93e923a78e08bf27e95a3d36d5a2363b79e564
otajisan <taji.taji.otajisan@gmail.com>
2019-11-11 03:26:35 +0900

    :tada: initial commit

---
+++ SendSlackNotification/src/main/kotlin/io/code/morning/SendSlackNotification.kt
@@ -0,0 +1,41 @@
+package io.code.morning
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.result.Result
+
+/**
+ * Handler for requests to Lambda function.
+ */
+class SendSlackNotification : RequestHandler<Any, Any> {
+
+    override fun handleRequest(input: Any, context: Context): Any {
+        return sendSlackMessage()
+    }
+
+    fun sendSlackMessage() {
+        val mapper = jacksonObjectMapper()
+        val webhookUrl: String = "https://hooks.slack.com/services/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"
+        val body: String = mapper.writeValueAsString(SlackMessage(
+                text = "Hello Lambda"
+        ))
+
+        val (_, _, response) = Fuel.post(webhookUrl).body(body).responseString()
+
+        when (response) {
+            is Result.Failure -> {
+                response.getException().toString()
+            }
+            is Result.Success -> {
+                response.get()
+            }
+        }
+    }
+
+    data class SlackMessage(
+            @JsonProperty("text") val text: String
+    )
+}
